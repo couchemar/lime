1d6bc0b55a9c6d728d03d39a269bd51aa89945ae
Andrey Pavlov <couchemar@yandex.ru>
2014-06-13 20:30:03 +0400

    Use stack

--- lib/lime/cli.ex
+++ lib/lime/cli.ex
@@ -2,6 +2,9 @@ defmodule Lime.CLI do
 
   require SandCat
 
+  @derive [Access]
+  defstruct stack: [], counter: 1
+
   def start do
     :user_drv.start([:"tty_sl -c -e", {Lime.CLI, :boot, []}])
   end
@@ -11,13 +14,26 @@ defmodule Lime.CLI do
   end
 
   defp start_server do
-     server_loop
+    struct(__MODULE__) |> server_loop
   end
 
-  def server_loop do
-    IO.write "Lime()> "
+  defp server_loop ctx do
+    prompt(ctx)
     q = Lime.IO.read
-    SandCat.run(q) |> IO.inspect
-    server_loop
+    new_stack = ctx[:stack] |> SandCat.compound(q) |> IO.inspect
+    ctx |> update_counter |> update_stack(new_stack) |> server_loop
   end
+
+  defp prompt %__MODULE__{counter: counter} do
+    IO.write "Lime(#{counter})> "
+  end
+
+  defp update_counter %__MODULE__{counter: counter}=ctx do
+    struct(ctx, counter: counter + 1)
+  end
+
+  defp update_stack(ctx, new_stack) do
+    struct(ctx, stack: new_stack)
+  end
+
 end
